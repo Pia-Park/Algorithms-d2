6c1e251d25b3a0e47f33c4b4f5c32ddda3049c71
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2020-09-15 10:59:25 -0700

    first answer

--- w2d2.js
+++ w2d2.js
@@ -6,17 +6,3 @@ return data.map(([n, count]) => n.toString().repeat(count)).join(',')
 console.log(repeatNumbers([[1, 10]]));
 console.log(repeatNumbers([[1, 2], [2, 3]]));
 
-
-
-
-
-
-//   let result = "";
-//   for(i = 0; i < data.length; i++){
-//       for(c = 0; c < data[i][1]; c++){
-//           result += data[i][0];
-//       }
-//       return result;
-//   }
-
-// };
